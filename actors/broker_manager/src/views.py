135784e6e310749f289e0597f8cd2184beec25e0
Debanjan Saha <debanjansaha2001@gmail.com>
2023-03-01 02:02:18 +0530

    endpoints added by aaditya

--- actors/broker_manager/src/views.py
+++ actors/broker_manager/src/views.py
@@ -31,9 +31,6 @@ class ProducerAPI(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument("topic_name", required=True, help="Topic name required")
         args = parser.parse_args()
-
-        if not master_queue.has_topic(args["topic_name"]):
-            master_queue.add_topic(args["topic_name"])
         
         producer_id = master_queue.add_producer(args["topic_name"])
         return {
@@ -48,8 +45,81 @@ class ConsumerAPI(Resource):
         args = parser.parse_args()
 
         consumer_id = master_queue.add_consumer(args["topic_name"])
-        return {
-            "status": "Success",
-            "consumer_id": consumer_id
-        }, 200
+        if consumer_id == None:
+            return {
+                "status": "Failure",
+                "message": "The topic does not exist"
+            }, 400
+        else:
+            return {
+                "status": "Success",
+                "consumer_id": consumer_id
+            }, 200
     
+class MessageAPI(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("consumer_id", required=True, help="Consumer id required")
+        parser.add_argument("topic_name", required=True, help="Topic name required")
+        args = parser.parse_args()
+        
+        message = master_queue.get_message(args["topic_name"], args["consumer_id"])
+        if message is None:
+            return {
+                "status": "Failure",
+                "message": "Unable to fetch message due to no more messages or broker is down"
+            }, 400
+        else:
+            return {
+                "status": "Success",
+                "message": message
+            }, 200     
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("producer_id", required=True, help="Producer id required")
+        parser.add_argument("topic_name", required=True, help="Topic name required")
+        parser.add_argument("message", required=True, help="Message required")
+        parser.add_argument("partition_id")
+        args = parser.parse_args()
+        
+        if master_queue.add_message(args["topic_name"], args["producer_id"], args["message"], args.get("partition_id")):
+            return {
+                "status": "Success"
+            }, 200
+        else:
+            return {
+                "status": "Failure",
+                "message": "Unable to add message due to invalid producer id or broker is down"
+            }, 400  
+
+class MessageSizeAPI(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("topic_name", required=True, help="Topic name required")
+        parser.add_argument("consumer_id", required=True, help="Consumer id required")
+        args = parser.parse_args()
+        
+        message_size = master_queue.find_size(args["topic_name"], args["consumer_id"])
+        if message_size is None:
+            return {
+                "status": "Failure",
+                "message": "Unable to fetch message size due to invalid input"
+            }, 400
+        else:
+            return {
+                "status": "Success",
+                "message_size": message_size
+            }, 200
+
+class BrokerAPI(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("broker_ip", required=True, help="Broker ip required")
+        parser.add_argument("broker_port", required=True, help="Broker port required")
+        args = parser.parse_args()
+        
+        master_queue.add_broker(args["broker_ip"], args["broker_port"])
+        return {
+            "status": "Success"
+        }, 200
